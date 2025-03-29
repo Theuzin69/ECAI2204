b01abdecdc9a22788dfbac0f8202498d29a08340
Theuzin69 <75169130+Theuzin69@users.noreply.github.com>
2025-03-29 10:47:31 -0300

    Update Guia3Exercicios.m

--- basicsMatlab/Guia3Exercicios.m
+++ basicsMatlab/Guia3Exercicios.m
@@ -1,18 +1,21 @@
+clc
 ## Autor: Matheus Lage Muzzi Cabral
 ## RA: 2023009628
-clc
 
 %%4.1
-#a função se chama "raizes"
-#a resposta com os valores pedidos foi:
-#2 e 1
+##o script se chama "raizes"
+##a resposta com os valores pedidos foi:
+##2 e 1
 %%
 
 %%7.2
+##graficodafuncao(-10,10,-10,10);
 %%
 
 %%7.7
+expx77(10,2)
 %%
 
 %%7.8
+expx78(9)
 %%
